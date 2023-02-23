a123759a6ce6ca296218a9ff0c50335548e23a9b
Hieuporo <111196224+Hieuporo@users.noreply.github.com>
2023-02-23 19:09:58 +0700

    first

---
+++ client/src/components/index.js
@@ -0,0 +1,27 @@
+import Logo from "./Logo";
+import FormRow from "./FormRow";
+import Alert from "./Alert";
+import Navbar from "./Navbar";
+import BigSidebar from "./BigSidebar";
+import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+import ChartsContainer from "./ChartsContainer";
+import StatsContainer from "./StatsContainer";
+import Loading from "./Loading";
+
+export {
+  Logo,
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  FormRow,
+  Alert,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  ChartsContainer,
+  StatsContainer,
+  Loading,
+};
---
+++ client/src/pages/Register.js
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
+import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const navigate = useNavigate();
+
+  const { user, isLoading, showAlert, displayAlert, setupUser } =
+    useAppContext();
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const toggleMember = () => {
+    setValues({
+      ...values,
+      isMember: !values.isMember,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!password || !email || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+    const currentUser = {
+      name,
+      email,
+      password,
+    };
+
+    if (isMember) {
+      setupUser({
+        currentUser,
+        endPoint: "login",
+        alertText: "Login successful! Redirecting...",
+      });
+    } else {
+      setupUser({
+        currentUser,
+        endPoint: "register",
+        alertText: "User Created! Redirecting...",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [user, navigate]);
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+          submit
+        </button>
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          disabled={isLoading}
+          onClick={() => {
+            setupUser({
+              currentUser: { email: "testUser@test.com", password: "secret" },
+              endPoint: "login",
+              alertText: "Loggin Successful! Redirecting...",
+            });
+          }}
+        >
+          {isLoading ? "loading..." : "demo app"}
+        </button>
+
+        <p>
+          {values.isMember ? "Not a member yet" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+}
+
+export default Register;
